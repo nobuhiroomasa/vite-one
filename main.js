f9dab0c4a9d05129ad5aa8b91f6ab80aef5bc585
大政信博 <oomasanobuhiro@oomasanobuhironoMacBook-Air.local>
2024-10-15 06:33:35 +0900

    Add new game

--- main.js
+++ main.js
@@ -4,21 +4,31 @@ import viteLogo from './public/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+    <header>
+      <ul class="header">
+          <li><a href="/vite-one/Asteroid/asteroid.htm">Asteroid</a></li>
+          <li><a href="/vite-one/Blocks/blocks.htm">Blocks</a></li>
+          <li><a href="/vite-one/MineSweeper/minesweeper.html">MineSweeper</a></li>
+          <li><a href="/vite-one/Cave/cave.html">Cave</a></li>
+          <li><a href="/vite-one/MissileCommand/missilecommand.htm">MissileCommand</a></li>
+          <li><a href="/vite-one/Qix/Qix.htm">Qix</a></li>
+          <li><a href="/vite-one/Invader/SpaceInvader.htm">Invader</a></li>
+          <li><a href="/vite-one/Memorizer/memorizer.html">Memorizer</a></li>
+          <li><a href="/vite-one/SnakeBite/SnakeBite.html">SnakeBite</a></li>
+          <li><a href="/vite-one/Tetris/Tetris.htm">Tetris</a></li>
+          <li><a href="/vite-one/index.html">Top</a></li>
+      </ul>
+      <div class="account-container">
+          <div class="account-name" id="account-name"></div> <!-- アカウント名を表示する部分 -->
+          <div class="dropdown-menu" id="dropdown-menu">
+              <div class="nickname-input">
+                  <input type="text" id="nickname" placeholder="ニックネームを入力" />
+                  <button id="save-nickname">保存</button>
+              </div>
+          </div>
+      </div>
+      
+    </header>
 `
 
 setupCounter(document.querySelector('#counter'))
