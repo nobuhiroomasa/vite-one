034d4de439a88a40a3f2f0faf57e760b6fa09cf2
nobuhiroomasa <tibi.t.2017.3.n@icloud.com>
2024-10-15 12:09:13 +0900

    ファイルの修正

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
-import javascriptLogo from './javascript.svg'
-import viteLogo from './public/vite.svg'
+
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
