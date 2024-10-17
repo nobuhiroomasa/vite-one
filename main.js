fe453bcdefbc2725143e69ea27605185ee66495c
大政信博 <oomasanobuhiro@oomasanobuhironoMacBook-Air.local>
2024-10-18 00:16:07 +0900

    ファイルの修正

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-    <header>
+document.querySelector('#header').innerHTML = `
+    <header >
       <ul class="header">
-          <li><a href="/vite-one/Asteroid/asteroid.htm">Asteroid</a></li>
-          <li><a href="/vite-one/Blocks/blocks.htm">Blocks</a></li>
-          <li><a href="/vite-one/MineSweeper/minesweeper.html">MineSweeper</a></li>
-          <li><a href="/vite-one/Cave/cave.html">Cave</a></li>
-          <li><a href="/vite-one/MissileCommand/missilecommand.htm">MissileCommand</a></li>
-          <li><a href="/vite-one/Qix/Qix.htm">Qix</a></li>
-          <li><a href="/vite-one/Invader/SpaceInvader.htm">Invader</a></li>
-          <li><a href="/vite-one/Memorizer/memorizer.html">Memorizer</a></li>
-          <li><a href="/vite-one/SnakeBite/SnakeBite.html">SnakeBite</a></li>
-          <li><a href="/vite-one/Tetris/Tetris.htm">Tetris</a></li>
+          <li class="asteroid"><a href="/vite-one/Asteroid/asteroid.htm">Asteroid</a></li>
+          <li class="Blocks"><a href="/vite-one/Blocks/blocks.htm">Blocks</a></li>
+          <li class="MineSweeper"><a href="/vite-one/MineSweeper/minesweeper.html">MineSweeper</a></li>
+          <li class="Cave"><a href="/vite-one/Cave/cave.html">Cave</a></li>
+          <li class="MissileCommand"><a href="/vite-one/MissileCommand/missilecommand.htm">MissileCommand</a></li>
+          <li class="Qix"><a href="/vite-one/Qix/Qix.htm">Qix</a></li>
+          <li class="Invader"><a href="/vite-one/Invader/SpaceInvader.htm">Invader</a></li>
+          <li class="Memorizer"><a href="/vite-one/Memorizer/memorizer.html">Memorizer</a></li>
+          <li class="SnakeBite"><a href="/vite-one/SnakeBite/SnakeBite.html">SnakeBite</a></li>
+          <li class="Tetris"><a href="/vite-one/Tetris/Tetris.htm">Tetris</a></li>
           <li><a href="/vite-one/index.html">Top</a></li>
       </ul>
 
